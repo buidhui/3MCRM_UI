dcfca00ebdf2b6b45d784a3cd0a6bb4efeb82723
Bui Duc Huy <buidhui@gmail.com>
2019-08-21 09:32:29 +0700

    update WeekChart

--- src/Components/Chart/WeekChart.js
+++ src/Components/Chart/WeekChart.js
@@ -1,24 +1,81 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
 import url from "../url";
 import axios from "axios";
 
 class WeekChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
+  state = {
+    conversion: [],
+    dataLine: {
+      labels: [],
+      datasets: [
+        {
+          label: "Đầu mối thành tiềm năng",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgb(255, 255, 255, 0.1)",
+          borderColor: "rgb(236, 112, 99)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(236, 112, 99)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 5,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(255, 255, 255)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        },
+        {
+          label: "Tiềm năng thành khách hàng",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgb(255, 255, 255, 0.1)",
+          borderColor: "rgb(247, 220, 111)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(247, 220, 111)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 5,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(255, 255, 255)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        },
+        {
+          label: "Đầu mối thành khách hàng",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgba(255, 255, 255, 0.1)",
+          borderColor: "rgb(88, 214, 141)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(88, 214, 141)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 5,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(255, 255, 255)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        }
+      ]
+    }
+  };
   componentDidMount() {
     axios({
       method: "get",
@@ -26,53 +83,68 @@ class WeekChart extends React.Component {
     })
       .then(respone => {
         this.setState({
-          data: respone.data
+          conversion: respone.data
         });
-        // console.log(respone.data);
       })
       .catch(error => {
         console.log(error);
       });
   }
+  pushLabel = (conversion, labelArr) => {
+    if (conversion.length !== 0) {
+      for (var i = 0; i < conversion.length; i++) {
+        var date = conversion[i].date;
+        var date1 = date.slice(5, 7) + "/" + date.slice(0, 4);
+        labelArr.push(date1);
 
+        // console.log(date1);
+      }
+    }
+  };
+  pushData0 = (conversion, dataArr) => {
+    if (conversion.length !== 0) {
+      for (var i = 0; i < conversion.length; i++) {
+        var date0 =
+          (conversion[i].leadToOpportunity / conversion[i].totalLead) * 100;
+        dataArr.push(Math.round(date0));
+      }
+    }
+  };
+  pushData1 = (conversion, data1) => {
+    if (conversion.length !== 0) {
+      for (var i = 0; i < conversion.length; i++) {
+        var date1 =
+          (conversion[i].opportunityToCustomer /
+            conversion[i].totalOpportunity) *
+          100;
+
+        data1.push(Math.round(date1));
+      }
+    }
+  };
+  pushData2 = (conversion, data2) => {
+    if (conversion.length !== 0) {
+      for (var i = 0; i < conversion.length; i++) {
+        var date2 = (conversion[i].newCustomer / conversion[i].totalLead) * 100;
+
+        data2.push(Math.round(date2));
+      }
+    }
+  };
   render() {
-    const { data } = this.state;
-    // console.log("datsadasdasda");
+    const { conversion } = this.state;
+    this.pushLabel(conversion, this.state.dataLine.labels);
+    this.pushData0(conversion, this.state.dataLine.datasets[0].data);
+    this.pushData1(conversion, this.state.dataLine.datasets[1].data);
+    this.pushData2(conversion, this.state.dataLine.datasets[2].data);
 
-    // console.log(data);
     return (
       <MDBContainer>
-        <BarChart
-          className="chart"
-          width={750}
-          height={550}
-          data={data}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar
-            name="KH Đầu Mối"
-            dataKey="totalLead"
-            stackId="a"
-            fill="#8884d8"
-          />
-          <Bar
-            name="KH Tiềm Năng"
-            dataKey="totalOpportunity"
-            stackId="a"
-            fill="#82ca9d"
-          />
-          <Bar
-            name="KH Chính Thức"
-            dataKey="newCustomer"
-            stackId="a"
-            fill="#ff4f68"
-          />
-        </BarChart>
+        <Line
+          height={255}
+          data={this.state.dataLine}
+          options={{ responsive: true }}
+        />
       </MDBContainer>
     );
   }
