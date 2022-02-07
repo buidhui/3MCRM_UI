2d374fb463200d7d8a79310d350b9c57a0208860
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-07 11:02:46 +0800

    Delete src directory

--- src/Components/url.js
+++
@@ -1,7 +0,0 @@
-// const url = "http://192.168.10.10:8080";
-const url = "http://172.104.164.101:8080";
-// const url = "http://192.168.10.22:8080";
-
-// const url = "http://localhost:9000";
-
-export default url;
